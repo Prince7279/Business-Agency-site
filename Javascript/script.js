d9d1e1275c3aa8bd907398f865f6600e398a1e5b
Bisha4225 <kumarbishal4225@gmail.com>
2023-02-25 14:33:38 +0530

    Style changes

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,3 +8,48 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("change");
 });
 // End of Navbar
+
+// Section 2 Video
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons button i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = ".7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  console.log(video.currentTime, video.duration);
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+});
+// End of Section 2 Video
+var Swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
